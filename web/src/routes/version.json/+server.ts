8697be35b2152eb545b1dff706ad00b7b7bde18d
Kehinde Egunjobi <kehindeegunjobi93@yahoo.com>
2026-09-17 13:57:11 +0100

    fix: disable prerender on version.json and add fallback version data for container build

--- web/src/routes/version.json/+server.ts
+++ web/src/routes/version.json/+server.ts
@@ -2,12 +2,21 @@ import { json } from "@sveltejs/kit";
 import { getCommit, getBranch, getRemote, getVersion } from "@imput/version-info";
 
 export async function GET() {
-    return json({
-        commit: await getCommit(),
-        branch: await getBranch(),
-        remote: await getRemote(),
-        version: await getVersion()
-    });
+    try {
+        return json({
+            commit: await getCommit(),
+            branch: await getBranch(),
+            remote: await getRemote(),
+            version: await getVersion()
+        });
+    } catch {
+        return json({
+            commit: "main",
+            branch: "main",
+            remote: "kehindeegunjobi93/online-downloader",
+            version: "1.0.0"
+        });
+    }
 }
 
-export const prerender = true;
+export const prerender = false;
